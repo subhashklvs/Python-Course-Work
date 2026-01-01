c738bb07cc0370876755b1845c0cafed1a9e40ca
subhashklvs <subhashklvs@gmail.com>
2026-01-01 22:41:53 +0530

    updated

--- Assignments/1.Train Tickets Booking.py
+++ Assignments/1.Train Tickets Booking.py
@@ -24,31 +24,3 @@ print("Train Ticket Reservation Details in Tuple:",Reservation_train_tickets_det
 print("Train Ticket Reservation Details in Set:",Reservation_train_tickets_details_set)
 print("Train Ticket Reservation Details in Dictionary:",Reservation_train_tickets_details_dict)#Train Ticket Reservation Details Management System
 
-# or # 
-
-# 1. int – User ID
-user_id = 102345
-
-# 2. str – User name
-user_name = "Sirimilla Swetha "
-
-# 3. float – Wallet balance
-wallet_balance = 1500.75
-
-# 4. list – Transaction history
-transaction_history = ["Recharge ₹199", "Bill Payment ₹450"]
-
-# 5. tuple – Bank account details (fixed data)
-bank_details = ("SBI Bank", "XXXX1234")
-
-# 6. set – Unique offers applied
-offers_applied = {"Cashback10", "NewUserOffer"}
-
-# 7. dict – PhonePe user profile
-phonepe_profile = {
-    "User ID": user_id,
-    "Name": user_name,
-    "Balance": wallet_balance,
-    "Bank": bank_details
-}
-
