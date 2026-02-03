f12d93c007b8ac0e0c3dae83264e71aa01f20642
subhashklvs <subhashklvs@gmail.com>
2026-02-03 10:15:19 +0530

    updated

--- Weekly Test/28-01-26 oops, Exception handling,File handling.py
+++ Weekly Test/28-01-26 oops, Exception handling,File handling.py
@@ -81,3 +81,15 @@ language = input()
 dev = Developer(name, salary, language)
 dev.display_details()
 
+#
+a=0
+b=1
+for i in range(8):
+    print(a,end=" ")
+    a,b=b,a+b
+
+a=5
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+    print(fact)
