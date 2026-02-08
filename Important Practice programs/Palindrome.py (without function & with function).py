318f55fd61c3e5c20506e06bafbae0ef6991d987
subhashklvs <subhashklvs@gmail.com>
2026-02-08 12:58:28 +0530

    updated

---
+++ Important Practice programs/Palindrome.py (without function & with function).py
@@ -0,0 +1,26 @@
+# n=121
+# temp=n
+# rev=0
+# while n>0:
+#     rem=n%10
+#     rev=rev*10+rem
+#     n=n//10
+# if temp==rev:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+    
+# with function :
+
+def is_palindrome(n):
+    temp=n
+    rev=0
+    while n>0:
+        rem=n%10
+        rev=rev*10+rem
+        n=n//10
+    if temp==rev:
+        return "palindrome"
+    else:
+        return "not palindrome"
+print(is_palindrome(121))
