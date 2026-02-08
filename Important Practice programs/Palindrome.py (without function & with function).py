90e849552bbec97310f772ac1444e9abe845c533
subhashklvs <subhashklvs@gmail.com>
2026-02-08 23:20:17 +0530

    updated

--- Important Practice programs/Palindrome.py (without function & with function).py
+++ Important Practice programs/Palindrome.py (without function & with function).py
@@ -1,14 +1,15 @@
-# n=121
-# temp=n
-# rev=0
-# while n>0:
-#     rem=n%10
-#     rev=rev*10+rem
-#     n=n//10
-# if temp==rev:
-#     print("palindrome")
-# else:
-#     print("not palindrome")
+# without function :
+n=121
+temp=n
+rev=0
+while n>0:
+    rem=n%10
+    rev=rev*10+rem
+    n=n//10
+if temp==rev:
+    print("palindrome")
+else:
+    print("not palindrome")
     
 # with function :
 
---
+++ Important Practice programs/factioral (without function and with function).py
@@ -0,0 +1,14 @@
+# without function :
+n=5
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+    
+# with function :
+def factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+        return fact
+print(factorial(5))
